db0cad7bd9c5bc76426331365403478a6620e0d6
Joshua Carrier <jcarrier0026@gmail.com>
2021-02-16 23:29:59 -0500

    Made a few changes to add employee.
    I was playing around with the new code for fun and i realized you couldn't add more than one name at a time (No first and last name). Just made a simple change to be able to add full names.

--- src/employee_database.cpp
+++ src/employee_database.cpp
@@ -1,9 +1,25 @@
 #include <fstream>
 #include <iostream>
 #include <istream>
+#include <sstream>
 #include <string>
 #include <vector>
-#include <sstream>
+
+// Employee names.
+std::string employee;
+std::vector<std::string> employees;
+std::string fullname;
+std::string user_input;
+
+void AddEmployee() {
+  std::cout << "Enter new employee's first name:" << std::endl;
+  std::cin >> fullname;
+  fullname += " ";
+  std::cout << "Enter new employee's second name:" << std::endl;
+  std::cin >> user_input;
+  fullname.append(user_input);
+  employees.push_back(fullname);
+}
 
 int main() {
   const std::string employees_file = "employees.txt";
@@ -11,16 +27,11 @@ int main() {
 
   std::ifstream employees_file_stream(employees_file);
 
-  // Employee names.
-  std::string employee;
-  std::vector<std::string> employees;
-
   // Read in the employee names.
   while (std::getline(employees_file_stream, employee)) {
     employees.push_back(employee);
   }
 
-  std::string user_input;
   std::cout
       << "Welcome to the employee database. Please log in with your username"
       << std::endl;
@@ -40,34 +51,32 @@ int main() {
   std::cout << "  2. Add employee" << std::endl;
   std::cin >> user_input;
   std::cout << std::endl;
-  
+
   std::stringstream input_number(user_input);
   int input_number_as_int;
-  
+
   input_number >> input_number_as_int;
 
-  switch(input_number_as_int){
-	case 1: 
-  for (const auto &e : employees) {
-    std::cout << e << std::endl;
-  }break;
-	case 2:
-  std::cout << "Enter new employee name." << std::endl; 
-  std::cin >> user_input;  
-  employees.push_back(user_input);
-  break;
-	default:
-  std::cout << "Invalid input" << std::endl;
-  return -1;
+  switch (input_number_as_int) {
+  case 1:
+    for (const auto &e : employees) {
+      std::cout << e << std::endl;
+    }
+    break;
+  case 2:
+    AddEmployee();
+    break;
+  default:
+    std::cout << "Invalid input" << std::endl;
+    return -1;
   }
-  	
+
   std::ofstream employees_file_ostream;
   employees_file_ostream.open(employees_file);
   for (const auto &e : employees) {
-    	employees_file_ostream << e << std::endl;
+    employees_file_ostream << e << std::endl;
   }
   employees_file_ostream.close();
-
 
   return 0;
 }
