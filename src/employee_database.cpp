ba7b40461c4b01901c792c0313ca76e60e34c5bf
Joshua Carrier <jcarrier0026@gmail.com>
2021-02-28 22:59:20 -0500

    Added an output to file function.
    We can now rewrite to the file at the end of the program. AddEmployee now works as intended.

--- src/employee_database.cpp
+++ src/employee_database.cpp
@@ -1,54 +1,67 @@
+#include "EmployeeRecord.h"
 #include <bits/stdc++.h>
 #include <fstream>
 #include <iostream>
 #include <vector>
-#include "EmployeeRecord.h"
 
 vector<EmployeeRecord> EmployeeData;
+string filename = "employees.txt";
 
-void doInputEmployeeDataFromFile(vector<EmployeeRecord>& A) {
-	string filename = "employees.txt";
-	ifstream infile(filename);
-	EmployeeRecord r;
+void doInputEmployeeDataFromFile(vector<EmployeeRecord> &A) {
+  ifstream infile(filename);
+  EmployeeRecord r;
   int dataSize;
 
-	if (!infile) {
-		cout << "Unable to open file";
-		exit(1); // terminate with error
-	}
-	if (infile.is_open()) {
-		// The first item in the data file is an integer
-		// it represents how many records are in the data file.
-		infile >> dataSize;
-		for (int j = 0; j < dataSize; j++) {
-			infile >> r.employeeId;
-			infile >> r.firstName;
-			infile >> r.lastName;
-			infile >> r.jobTitle;
-			infile >> r.manager;
-			infile >> r.departmentNumber;
-			infile >> r.payRate;
-			infile >> r.salaried;
+  if (!infile) {
+    cout << "Unable to open file";
+    exit(1); // terminate with error
+  }
+  if (infile.is_open()) {
+    // The first item in the data file is an integer
+    // it represents how many records are in the data file.
+    infile >> dataSize;
+    for (int j = 0; j < dataSize; j++) {
+      infile >> r.employeeId;
+      infile >> r.firstName;
+      infile >> r.lastName;
+      infile >> r.jobTitle;
+      infile >> r.manager;
+      infile >> r.departmentNumber;
+      infile >> r.payRate;
+      infile >> r.salaried;
       A.push_back(r);
-		} // end for
-		infile.close();
-	} // end if
+    } // end for
+    infile.close();
+  } // end if
 } // doInputAirportDataFromFile>
 
-void outputEmployeeSequence(vector<EmployeeRecord>& A){
-    for (int i=0; i<A.size(); i++) { 
-        cout << '\t' << A[i] << endl;
-    } // for
-    cout << endl;
+void doOutputEmployeeDataToFile(vector<EmployeeRecord> &A) {
+  ofstream ofile;
+  ofile.open(filename);
+  ofile << A.size() << endl;
+  for (int i = 0; i < A.size(); i++) {
+    ofile << A[i].employeeId << " " << A[i].firstName << " " << A[i].lastName
+          << " " << A[i].jobTitle << " " << A[i].manager << " "
+          << A[i].departmentNumber << " " << A[i].payRate << " "
+          << A[i].salaried << endl;
+  }
+  ofile.close();
+}
+
+void outputEmployeeSequence(vector<EmployeeRecord> &A) {
+  for (int i = 0; i < A.size(); i++) {
+    cout << '\t' << A[i] << endl;
+  } // for
+  cout << endl;
 } // outputAirportSequence
 
-void AddEmployee(vector<EmployeeRecord>& A) {
+void AddEmployee(vector<EmployeeRecord> &A) {
   EmployeeRecord newRecord;
   string stringWriter;
-  int intWriter; 
+  int intWriter;
 
   // EmployeeID is incremented by 1
-  newRecord.employeeId = A[A.size()-1].employeeId + 1;
+  newRecord.employeeId = A[A.size() - 1].employeeId + 1;
 
   std::cout << "Enter new employee first name: " << std::endl;
   cin >> stringWriter;
@@ -68,20 +81,20 @@ void AddEmployee(vector<EmployeeRecord>& A) {
 
   std::cout << "Enter new employee department number: " << std::endl;
   cin >> intWriter;
-  newRecord.departmentNumber = intWriter; 
+  newRecord.departmentNumber = intWriter;
 
   std::cout << "Enter new employee pay rate: " << std::endl;
   cin >> intWriter;
-  newRecord.payRate = intWriter; 
+  newRecord.payRate = intWriter;
 
   std::cout << "Is new employee salaried?: " << std::endl;
   cin >> stringWriter;
   newRecord.salaried = stringWriter;
-  
+
   A.push_back(newRecord);
 }
 
-void makeSelection(){
+void makeSelection() {
   string user_input;
   std::cout << "Welcome admin! What would you like to do?" << std::endl;
   std::cout << std::endl;
@@ -105,12 +118,13 @@ void makeSelection(){
     break;
   default:
     std::cout << "Invalid input" << std::endl;
-    //return -1;
+    // return -1;
   }
 }
 
 int main() {
   doInputEmployeeDataFromFile(EmployeeData);
   makeSelection();
+  doOutputEmployeeDataToFile(EmployeeData);
   return 0;
 }
