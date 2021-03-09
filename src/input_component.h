d0a4eeecc71097e13f7a36620edf6849463e20c0
Joshua Carrier <jcarrier0026@gmail.com>
2021-03-08 20:24:58 -0500

    Add comments.
    Added comments to input_componenet and input_engine files.

--- src/input_component.h
+++ src/input_component.h
@@ -4,14 +4,20 @@
 #include <SDL2/SDL.h>
 #include <unordered_map>
 
+// IMPORTANT: InputComponent is a singleton!
 struct InputComponent {
+  // Map that holds all of the current held keys.
   std::unordered_map<SDL_Scancode, bool> held_keys;
+  // Map that holds all of the keys that were just pressed.
   std::unordered_map<SDL_Scancode, bool> pressed_keys;
+  // Holds all of the keys that have been released.
   std::unordered_map<SDL_Scancode, bool> released_keys;
 
+  // Get the instance of this singleton.
   static InputComponent &Get();
 
 private:
+  // This is the one instance of this object.
   static InputComponent *instance_;
 };
 #endif
--- src/input_engine.h
+++ src/input_engine.h
@@ -7,10 +7,17 @@
 
 class InputEngine {
 public:
+  // This function clears out the pressed_keys and released_keys maps in the
+  // InputComponent.
   void BeginNewFrame();
+  // This function tells the engine what to do if a key was released.
   void KeyUpEvent(SDL_Event &event);
+  // This function tells the engine what to do if a key was pressed.
   void KeyDownEvent(SDL_Event &event);
+  // This function checks to see if there has been a KeyUp, KeyDown, or Quit
+  // event.
   int CheckForInput();
+  // This function will determine which key pressed has the highest priority.
   int Update(LocationComponent &location);
 };
 #endif
