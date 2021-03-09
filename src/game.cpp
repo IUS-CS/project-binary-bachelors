fe76c6b6656098ce58b2440f08f60667493670f7
Joshua Carrier <jcarrier0026@gmail.com>
2021-03-08 22:55:33 -0500

    More comments.
    Added some comments to game.h and game.cpp

--- src/game.cpp
+++ src/game.cpp
@@ -16,6 +16,9 @@ void Game::Run() {
   GraphicsEngine graphics_engine;
   InputEngine input_engine;
   SpriteComponent sprite;
+  // Right now, the components are hard coded into the program and are not
+  // retrieved from the GameObject class. This is temporary and will be resolved
+  // soon.
   LocationComponent location;
   lonk.AddComponent(sprite);
   lonk.AddComponent(location);
@@ -32,8 +35,14 @@ void Game::Run() {
 
 void Game::LimitFrameRate() {
   int current_time_ms = SDL_GetTicks();
+  // This is the amount of time that has passed since the start of this frame.
   int elapsed_time_ms = current_time_ms - frame_start_time_ms;
-  if (elapsed_time_ms < 20) {
-    SDL_Delay(20 - elapsed_time_ms);
+  // The number 17 is used here because that is the amount of time that needs to
+  // pass for each frame so that the frame rate will be 60 FPS (One second is
+  // 1000 ms. 1000/17 = 59. So 17 is the number of ms we want to pass to get us
+  // close to 60 FPS). So if the loop finishes faster than 17 ms, we make the
+  // program wait to start the next frame.
+  if (elapsed_time_ms < 17) {
+    SDL_Delay(17 - elapsed_time_ms);
   }
 }
--- src/game.h
+++ src/game.h
@@ -13,8 +13,11 @@ public:
   // This function is the game. While in this function, the game is running.
   // When you leave this function, the game is closed.
   void Run();
+  // This function limits the frame rate to about 60 frames per second. The
+  // frame rate must be limited or else the game fluctuates in performance..
   void LimitFrameRate();
 
+  // This variable helps limit the frame rate.
   int frame_start_time_ms;
 };
 #endif
