574b695ef6091806c0613e50394306bb27889abc
Jonathan Hasty <jmhasty@iu.edu>
2021-03-14 00:22:43 -0500

    Added comments to code

--- src/health_engine.cpp
+++ src/health_engine.cpp
@@ -10,19 +10,16 @@ void HealthEngine::Run(GameObject &object){
         for (auto& hit_object : object.hit_box->objects_hit)
         {
             if (object.is_active){
-            CheckInvincibility(object);
-            //std::cout << "in2" << std::endl;
-            if (hit_object == HitBoxType::kMonster && object.health->health > 0 && !object.hit_box->is_invincible){
+              CheckInvincibility(object);
+              // Checks that player is alive and not invincible on monster collision.
+              if (hit_object == HitBoxType::kMonster && object.health->health > 0 && !object.hit_box->is_invincible){
                 object.health->health = object.health->health-1;
                 object.hit_box->is_invincible = true;
                 object.hit_box->time_since_last_hit_ms = SDL_GetTicks();
-                std::cout << " got hit!" << std::endl;
-            }
-            if (object.health->health == 0) {
+              }
+              if (object.health->health == 0) {
                 object.is_active = false;
-                std::cout << "You are dead!" << std::endl;
-                std::cout << object.is_active << std::endl;
-            }
+              }
             }
         }
     }
@@ -31,10 +28,5 @@ void HealthEngine::Run(GameObject &object){
 void HealthEngine::CheckInvincibility(GameObject &object){
     int current_time_ms = SDL_GetTicks();
     int elapsed_time_ms = current_time_ms - object.hit_box->time_since_last_hit_ms;
-    if (elapsed_time_ms < 2000){
-        
-    }
-    else {
-        object.hit_box->is_invincible = false;
-    }
+    if (elapsed_time_ms >= 2000) object.hit_box->is_invincible = false;
 }
--- src/health_engine.h
+++ src/health_engine.h
@@ -2,11 +2,15 @@
 #define HEALTH_ENGINE_H
 #include "game_object.h"
 
+// This engine will determine object's remaining hit points.
 class HealthEngine {
 public:
   HealthEngine();
   ~HealthEngine();
   void Run(GameObject &object);
+
+  // This CheckInvincibility function is required; otherwise, an unintended amount of
+  // health will be immediately depleted upon collision. 
   void CheckInvincibility(GameObject &object);
 };
 #endif
